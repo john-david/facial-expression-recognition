2ae20a8dcfa96cf3c75cd04fc1d86cc84c59e668
John David <johndavidstl@yahoo.com>
2018-06-15 13:53:20 -0500

    Update show_images.py

--- show_images.py
+++ show_images.py
@@ -1,10 +1,5 @@
 
 
-from __future__ import print_function, division
-from builtins import range, input
-# Note: you may need to update your version of future
-# sudo pip install -U future
-
 import numpy as np
 import matplotlib.pyplot as plt
 
