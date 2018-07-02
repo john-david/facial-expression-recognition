75abacba91499d08a9a84b298a3805197d5af5ac
John David <johndavidstl@yahoo.com>
2018-07-02 12:44:43 -0500

    Add files via upload

--- util.py
+++ util.py
@@ -2,8 +2,6 @@
 
 from __future__ import print_function, division
 from builtins import range
-# Note: you may need to update your version of future
-# sudo pip install -U future
 
 import numpy as np
 import pandas as pd
